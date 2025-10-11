721d5db088b1235699dca5afa09299790786eda9
Carlos Zanotti <cezanotti@gmail.com>
2025-10-11 12:22:20 -0300

    create Rank component and add animated background

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import React, { Component } from 'react';
+import ParticlesBg from 'particles-bg';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
@@ -10,6 +12,7 @@ function App() {
 
   return (
     <div className='App'>
+      <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -1,5 +1,12 @@
 export default function Rank() {
     return(
-        <div>RANK</div>
+        <div>
+            <div className="white f3" style={{textAlign:'center'}}>
+                {'Chino, your current rank is...'}
+            </div>
+            <div className="white f1" style={{textAlign:'center'}}>
+                {'#5'}
+            </div>
+        </div>
     );
 }
