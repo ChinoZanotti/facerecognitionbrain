583b1240289077c313969f1525a25f2720faa194
Carlos Zanotti <cezanotti@gmail.com>
2025-10-23 17:38:03 -0300

    Comment out Clarifai API integration and disable related functionality for now

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,6 +1,6 @@
 import './ImageLinkForm.css';
 
-export default function ImageLinkForm() {
+export default function ImageLinkForm({ onInputChange, onButtonSubmit }) {
     return(
         <div>
             <p className="f3" style={{textAlign:'center'}}>
@@ -8,8 +8,8 @@ export default function ImageLinkForm() {
             </p>
             <div className="center">
                 <div className="form center">
-                    <input type="text" className="f4 pa2 w-70 center" />
-                    <button className="w-30 grow f4 link ph3 pv2 dib white">Detect</button>
+                    <input type="text" className="f4 pa2 w-70 center" onChange={onInputChange} />
+                    <button className="w-30 grow f4 link ph3 pv2 dib white" onClick={onButtonSubmit}>Detect</button>
                 </div>
             </div>
         </div>
